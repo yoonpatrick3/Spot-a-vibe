05ff5501551dd32df3a8e499f250e60f62d7f581
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-02-19 21:15:30 -0500

    ddl sql script created

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -1,2 +1,32 @@
 CREATE DATABASE SpotifySongs;
-USE DATABASE 
+USE SpotifySongs;
+
+CREATE TABLE Artist (
+    artist_id int NOT NULL AUTO_INCREMENT,
+    artist_name varchar(100) NOT NULL,
+    PRIMARY KEY(artist_id)
+);
+
+CREATE TABLE Songs (
+    id varchar(50) NOT NULL,
+    title varchar(100) NOT NULL,
+    artist_id int NOT NULL,
+    img_link varchar(100),
+    album varchar(100),
+    acousticness double,
+    danceability double,
+    duration_ms double,
+    energy double,
+    instrumentalness double,
+    key_scale double,
+    liveness double,
+    loudness double,
+    mode double,
+    speechiness double,
+    tempo double,
+    time_signature double,
+    valence double,
+    PRIMARY KEY (id),
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id) 
+);
+
