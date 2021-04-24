3baff58eb43e0b7ce2fd018518e7c5b58a25b90f
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-04-23 22:27:09 -0400

    update server address, database name, cacheing in app package.json
    updated cacheing in app package.json

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom'
 import Body from './components/Body'
 
-export const address = 'https://spot-ai-fy.herokuapp.com/'
+export const address = 'http://localhost:5000'
 
 
 function App() {
