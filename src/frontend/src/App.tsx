15bd54ffdcf8f6e73fb358600c2fb0d6a75c67eb
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-05-08 15:08:00 -0400

    updated server address

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -11,7 +11,7 @@ import Fab from '@material-ui/core/Fab';
 import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 import { SpotPhase } from './components/Spot'
 
-export const address = 'http://localhost:5000'
+export const address = 'https://spot-a-vibe.herokuapp.com'
 
 const useStyles = makeStyles((theme) => ({
   extendedIcon: {
